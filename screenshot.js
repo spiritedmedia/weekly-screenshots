3a4a667e4f01cf9915d3f345422a6901be222aba
Russell Heimlich <kingkool68@gmail.com>
2017-09-19 15:33:58 -0400

    Cleanup and comment

--- screenshot.js
+++ screenshot.js
@@ -1,64 +1,62 @@
-/**
- * Copyright 2017 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 
 const puppeteer = require('puppeteer');
 
-var theURL = process.argv[2];
-var thePath = process.argv[3];
+// The URL to screenshot
+const theURL = process.argv[2];
+// The path of where the image should be saved as determined by the PHP script
+const thePath = process.argv[3];
 
 (async() => {
-	const browser = await puppeteer.launch({
-            args: ["--no-sandbox", "--disable-setuid-sandbox"]
-        });
-	const page = await browser.newPage();
-	await page.setViewport({
-		width: 1440,
-		height: 1024,
-		deviceScaleFactor: 1,
-	});
-	await page.goto(theURL, {
-		waitUntil: 'networkidle',
-		networkIdleTimeout: 2000,
-	});
-	await page.screenshot({
-		path: thePath + '-desktop.jpg',
-		fullPage: true,
-		type: 'jpeg',
-	});
 
-	// Mobile
-	await page.setViewport({
-		width: 320,
-		height: 480,
-		deviceScaleFactor: 1,
-		isMobile: true,
-		hasTouch: true,
-	});
-	// Set iPhone 7 user agent to trigger mobile ads
-	await page.setUserAgent( 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2_1 like Mac OS X) AppleWebKit/602.4.6 (KHTML, like Gecko) Version/10.0 Mobile/14D27 Safari/602.1' );
-	await page.reload({
-		waitUntil: 'networkidle',
-		networkIdleTimeout: 2000,
-	});
-	await page.screenshot({
-		path: thePath + '-mobile.jpg',
-		fullPage: true,
-		type: 'jpeg',
-	});
-	browser.close();
+    // On the server we need to run in no-sandbox mode which could be bad
+    // See https://github.com/GoogleChrome/puppeteer/issues/290#issuecomment-327233672
+    const browser = await puppeteer.launch({
+            args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+    const page = await browser.newPage();
+
+    // Desktop
+    await page.setViewport({
+        width: 1440,
+        height: 1024,
+        deviceScaleFactor: 1,
+    });
+
+    await page.goto(theURL, {
+        waitUntil: 'networkidle',
+        networkIdleTimeout: 2000,
+    });
+
+    await page.screenshot({
+        path: thePath + '-desktop.jpg',
+        fullPage: true,
+        type: 'jpeg',
+    });
+
+    // Mobile
+    await page.setViewport({
+        width: 320,
+        height: 480,
+        deviceScaleFactor: 1,
+        isMobile: true,
+        hasTouch: true,
+    });
+
+    // Set iPhone 7 user agent to trigger mobile ads
+    await page.setUserAgent( 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2_1 like Mac OS X) AppleWebKit/602.4.6 (KHTML, like Gecko) Version/10.0 Mobile/14D27 Safari/602.1' );
+
+    await page.reload({
+        waitUntil: 'networkidle',
+        networkIdleTimeout: 2000,
+    });
+
+    await page.screenshot({
+        path: thePath + '-mobile.jpg',
+        fullPage: true,
+        type: 'jpeg',
+    });
+
+    browser.close();
+
 })();
--- screenshot.php
+++ screenshot.php
@@ -1,15 +1,22 @@
 <?php
+/**
+ * Download an offline-ready version of the URL
+ *
+ * @param  string $url The URL to download
+ * @param  string $dir The directory to where the downloaded files should go
+ */
 function capture_html( $url = '', $dir = '' ) {
 	$wget_arg_parts = array(
 		'--page-requisites' => '',
 		'--convert-links' => '',
 		'--span-hosts' => '',
-		'--no-clobber' => '',
 		'--directory-prefix' => $dir,
 		'-e' => '',
 		'robots' => 'off',
 	);
 	$wget_args = array();
+
+	// Construct the arguments for wget command
 	foreach ( $wget_arg_parts as $key => $arg ) {
 		if ( $arg ) {
 			$key .= '=' . escapeshellarg( $arg );
@@ -17,12 +24,16 @@ function capture_html( $url = '', $dir = '' ) {
 		$wget_args[] = $key;
 	}
 	$wget_args = implode( ' ', $wget_args );
-
-	// Download an offline-ready capture of the URL.
-	// print( "wget $wget_args " . escapeshellarg( $url ) );
 	exec( "wget $wget_args " . escapeshellarg( $url ), $output, $return );
 }
 
+/**
+ * Capture a full page screenshot of a given URL
+ * at a desktop size and a mobile size
+ *
+ * @param  string $url  The URL to download
+ * @param  string $path The path where the screenshot should be saved
+ */
 function capture_screenshot( $url = '', $path = '' ) {
 	$args = array_map( 'escapeshellarg', [
 		$url,
@@ -34,19 +45,18 @@ function capture_screenshot( $url = '', $path = '' ) {
 $screenshots = [
 	'billy-penn' => [
 		'homepage' => 'https://billypenn.com/',
-		'article' => 'https://billypenn.com/2017/09/18/phillys-holocaust-memorial-is-getting-a-7-million-overhaul/',
+		'article'  => 'https://billypenn.com/2017/09/18/phillys-holocaust-memorial-is-getting-a-7-million-overhaul/',
 	],
 	'the-incline' => [
 		'homepage' => 'https://theincline.com/',
-		'article' => 'https://theincline.com/2017/09/18/which-pittsburgh-neighborhoods-claim-the-most-sidewalk-parking-tickets/',
+		'article'  => 'https://theincline.com/2017/09/18/which-pittsburgh-neighborhoods-claim-the-most-sidewalk-parking-tickets/',
 	],
 	'denverite' => [
 		'homepage' => 'https://www.denverite.com/',
-		'article' => 'https://www.denverite.com/the-broncos-42775/',
+		'article'  => 'https://www.denverite.com/the-broncos-42775/',
 	],
 ];
 
-$the_start_time = microtime();
 $root = dirname( __FILE__ );
 foreach ( $screenshots as $site => $data ) {
 	foreach ( $data as $page_type => $url ) {
@@ -57,9 +67,9 @@ foreach ( $screenshots as $site => $data ) {
 		$filename = $site . '-' . $page_type . '-' . date( 'Y-m-d' );
 		$path = $dir . $filename;
 
-		echo '--------------------------' . PHP_EOL;
+		echo '-----------------------' . PHP_EOL;
 		echo  $site . ' ' . $page_type . PHP_EOL;
-		echo '--------------------------' . PHP_EOL;
+		echo '-----------------------' . PHP_EOL;
 
 		capture_screenshot( $url, $path );
 
@@ -70,5 +80,3 @@ foreach ( $screenshots as $site => $data ) {
 		capture_html( $url, $dir );
 	}
 }
-$the_end_time = microtime();
-echo 'Finished in ' . $the_end_time - $the_start_time . ' seconds.' . PHP_EOL;
