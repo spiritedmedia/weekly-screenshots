fb91acbbaa4956f19cef0ca719935bf05f4d3a00
System <systems@spiritedmedia.com>
2017-09-19 20:53:19 +0000

    Clean up cronjob script

--- cronjob.sh
+++ cronjob.sh
@@ -2,12 +2,17 @@
 
 # Run this screenshot script once a week
 # add to this users crontab via crontab -e
-# 0 */1 * * * php /var/www/weekly-screenshot.localhost/weekly-screenshots/cronjob.sh >>/var/www/weekly-screenshot.localhost/weekly-screenshots/cron.log 2>&1
+
+# Run the screnshot script at 4:30am every Wednesday
+# 15 4 * * 3 /var/www/weekly-screenshot.localhost/weekly-screenshots/cronjob.sh > /dev/null 2>&1
+
+cd /var/www/weekly-screenshot.localhost/weekly-screenshots
 
 # Run the script
-php screenshot.php
+/usr/bin/php screenshot.php
 
 # Add everything and commit
+
 git add -A
-git commit -m "Screenshots added."
+git commit -m "Screenshots added for $(date +%F)"
 git push origin master --force
